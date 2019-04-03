24216cb9b45b6616217910514bf142a8d367529c
Ktokolwiek <iksde@op.pl>
2019-04-03 11:13:44 +0200

    Added Pietro

---
+++ JavaApp/src/javaapp/Pietro.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapp;
+
+/**
+ *
+ * @author student
+ */
+public class Pietro implements Powierzchnia {
+    int iloscPomieszczen;
+    int numerPietra;    
+}
